e55ddb74e79636e108c544d6648c6d786ddd7098
ndvazly <nadav.azulay@gmail.com>
2021-04-05 17:07:31 +0300

    Fixes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from functools import lru_cache
 
 save_event = None
 interrupted = False
-
+clip_start = 0
 
 def imdisplay(imarray, screen=None):
     """Splashes the given image array on the given pygame screen."""
@@ -25,11 +25,6 @@ def custom_preview(
     clip,
     first_frame,
     fps=15,
-    audio=False,
-    audio_fps=22050,
-    audio_buffersize=3000,
-    audio_nbytes=2,
-    fullscreen=False,
 ):
     """
     Displays the clip in a window, at the given frames per second
@@ -52,48 +47,17 @@ def custom_preview(
     global interrupted
     global save_event
 
-    flags = 0
-
-    # compute and splash the first image
-    # screen = pg.display.set_mode(clip.size, flags)
-
-    # audio = audio and (clip.audio is not None)
-    #
-    # if audio:
-    #     # the sound will be played in parrallel. We are not
-    #     # parralellizing it on different CPUs because it seems that
-    #     # pygame and openCV already use several cpus it seems.
-    #
-    #     # two synchro-flags to tell whether audio and video are ready
-    #     video_flag = threading.Event()
-    #     audio_flag = threading.Event()
-    #     # launch the thread
-    #     audiothread = threading.Thread(
-    #         target=clip.audio.preview,
-    #         args=(audio_fps, audio_buffersize, audio_nbytes, audio_flag, video_flag),
-    #     )
-    #     audiothread.start()
-
-    # img = clip.get_frame(0)
-    # img = first_frame;
     imdisplay(first_frame, screen)
-    # if audio:  # synchronize with audio
-    #     video_flag.set()  # say to the audio: video is ready
-    #     audio_flag.wait()  # wait for the audio to be ready
-
-    result = []
 
     t0 = time.time()
-    for t in np.arange(1.0 / fps, clip.duration - 0.001, 1.0 / fps):
-
-        img = clip.get_frame(t)
-
+    for t in np.arange(1.0 / fps, clip.duration - 0.001 - clip_start, 1.0 / fps):
         for clip_event in pg.event.get():
             if clip_event.type == pg.KEYDOWN:
                 interrupted = True
                 save_event = clip_event
-                return result
-
+                return
+                # return result
+        img = clip.get_frame(t - clip_start)
         t1 = time.time()
         time.sleep(max(0, t - (t1 - t0)))
         imdisplay(img, screen)
@@ -106,8 +70,8 @@ res = (540, 960)
 clips = []
 clip_names = []
 clips_first_frame = []
-# clip_keys = {97: 0, 98: 1, 99: 2, 100: 3, 101: 4, 102: 5}
 clip_keys = {122: 0, 120: 1, 99:2, 118:3, 98:4, 97:5, 115:6, 100:7, 102:8}
+
 
 def LoadFolder(path):
     global clip_names
@@ -118,7 +82,8 @@ def LoadFolder(path):
     clip_names += glob.glob(path + "/*.avi")
     for clip_i in range(len(clip_names)):
         clips.append(VideoFileClip(clip_names[clip_i], target_resolution=res, audio=False))
-        clips_first_frame.append(clips[clip_i].get_frame(0))
+        # clips[clip_i] = clips[clip_i].subclip()
+        clips_first_frame.append(clips[clip_i].get_frame(clip_start))
 
 
 def HandleEvent(event):
@@ -158,13 +123,3 @@ while True:
         HandleEvent(save_event)
 
 pg.quit()
-# pg.init()
-
-# elif event.type == pg.MOUSEBUTTONDOWN:
-# x, y = pg.mouse.get_pos()
-# rgb = img[y, x]
-# result.append({"time": t, "position": (x, y), "color": rgb})
-# print(
-#     "time, position, color : ",
-#     "%.03f, %s, %s" % (t, str((x, y)), str(rgb)),
-# )
