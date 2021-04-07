90d703cc322af1a9cd64bef13cf8c0c200b5c79b
Nadav Azulay <nadav.azulay@gmail.com>
2021-04-07 11:48:22 +0300

    Play Seq

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from moviepy.editor import *
 import pygame as pg
-import threading
 import time
 import numpy as np
 import tkinter as tk
 import glob
-from functools import lru_cache
 
 save_event = None
 interrupted = False
 clip_start = 0
 start_time = 0
+
 
 def imdisplay(imarray, screen=None):
     """Splashes the given image array on the given pygame screen."""
@@ -22,53 +21,43 @@ def imdisplay(imarray, screen=None):
 
 
 def custom_preview(
-    screen,
-    clip,
-    first_frame,
+    screen_buf,
+    clip_object,
     fps=15,
+    add_key_frame=True,
+    duration=None
 ):
-    """
-    Displays the clip in a window, at the given frames per second
-    (of movie) rate. It will avoid that the clip be played faster
-    than normal, but it cannot avoid the clip to be played slower
-    than normal if the computations are complex. In this case, try
-    reducing the ``fps``.
-    Parameters
-    ----------
-    fps
-      Number of frames per seconds in the displayed video.
-    audio
-      ``True`` (default) if you want the clip's audio be played during
-      the preview.
-    audio_fps
-      The frames per second to use when generating the audio sound.
-    fullscreen
-      ``True`` if you want the preview to be displayed fullscreen.
-    """
     global interrupted
     global save_event
 
-    imdisplay(first_frame, screen)
+    clip = clip_object["clip"]
+    imdisplay(clip_object["first_frame"], screen_buf)
 
     t0 = time.time()
     step_size = 1.0 / fps
-    frame_num = (t0 - start_time) / step_size
-    print(f'step size: {step_size}\n #frames: {round(frame_num)}')
-    # in_point = round(how_many_frames) * step_size
-    in_point = round(frame_num)
+    if duration is None:
+        duration = clip.duration
+
     # print(in_point)
-    AddKeyFrame(in_point,clip)
-    for t in np.arange(step_size, clip.duration - 0.001 - clip_start, step_size):
+    if add_key_frame:
+        frame_num = (t0 - start_time) / step_size
+        print(f'step size: {step_size}\n #frames: {round(frame_num)}')
+        in_point = round(frame_num)
+        AddKeyFrame(in_point, clip_object)
+
+    for t in np.arange(step_size, duration - 0.001 - clip_start, step_size):
         for clip_event in pg.event.get():
             if clip_event.type == pg.KEYDOWN:
                 interrupted = True
                 save_event = clip_event
+                if add_key_frame:
+                    out_time = time.time() - t0
+                    SetKeyFrameDuration(in_point, out_time)
                 return
-                # return result
         img = clip.get_frame(t - clip_start)
         t1 = time.time()
         time.sleep(max(0, t - (t1 - t0)))
-        imdisplay(img, screen)
+        imdisplay(img, screen_buf)
 
 
 root = tk.Tk()
@@ -76,28 +65,34 @@ root.geometry('400x400')
 
 res = (540, 960)
 clips = []
-clip_names = []
-clips_first_frame = []
 keyframes = {}
 clip_keys = {122: 0, 120: 1, 99:2, 118:3, 98:4, 97:5, 115:6, 100:7, 102:8}
 
 
 def LoadFolder(path):
-    global clip_names
     global res
-    clip_names += glob.glob(path + "/*.mov")
-    clip_names += glob.glob(path + "/*.wmv")
-    clip_names += glob.glob(path + "/*.mp4")
-    clip_names += glob.glob(path + "/*.avi")
-    for clip_i in range(len(clip_names)):
-        clips.append(VideoFileClip(clip_names[clip_i], target_resolution=res, audio=False))
-        # clips[clip_i] = clips[clip_i].subclip()
-        clips_first_frame.append(clips[clip_i].get_frame(clip_start))
+    clip_files = []
+    clip_files += glob.glob(path + "/*.mov")
+    clip_files += glob.glob(path + "/*.wmv")
+    clip_files += glob.glob(path + "/*.mp4")
+    clip_files += glob.glob(path + "/*.avi")
+    for clip_i in range(len(clip_files)):
+        new_clip = {"name": clip_files[clip_i]}
+        new_clip["clip"] = VideoFileClip(new_clip["name"], target_resolution=res, audio=False)
+        new_clip["first_frame"] = new_clip["clip"].get_frame(clip_start)
+        clips.append(new_clip)
 
 
 def AddKeyFrame(in_time, clip_object):
-    keyframes[in_time] = clip_object
-    print(f'Added frame {len(keyframes)} | in: {in_time} clip: {clip_object}')
+    keyframes[in_time] = {"clip_object": clip_object}
+    # keyframes[in_time]["duration"] = 0.2
+    keyframes[in_time]["duration"] = clip_object["clip"].duration
+    print(f'Added frame {len(keyframes)} | in: {in_time} ')
+
+
+def SetKeyFrameDuration(in_time, duration):
+    print(f"SetKeyFrameDuration: {duration}")
+    keyframes[in_time]["duration"] = duration
 
 
 def StartRecording():
@@ -109,26 +104,21 @@ def PlayRecording():
     global keyframes
     frame_rate = 30
     step = (1/frame_rate)
-    duration = 300
-    playhead = 0
-    print('Playing Sequence')
-    # for t in np.arange(0, duration - 0.001, step_size):
-    for p in range(duration):
-        if keyframes.get(p):
-            print(f'{p}: play {keyframes[p]}')
-            # custom_preview(screen=screen, clip=keyframes[p], first_frame=clips_first_frame[0], fps=frame_rate)
-        time.sleep(step)
-
-    # print (keyframes)
-    # while playhead < duration:
-    #     if keyframes.get(playhead):
-    #         print(f'{playhead}: play {keyframes[playhead]}')
-        # else:
-        #     print(playhead)
-        # playhead += step
-    # for t in np.arange(0, duration, step):
-    #     if t in keyframes:
-    #         print(f'{t}: play {keyframes[t]}')
+    duration = 90
+    play_head = 0
+    # print('Playing Sequence')
+    while play_head < duration:
+        if keyframes.get(play_head):
+            # print(f'{play_head}: play')
+            t_before_clip = time.time()
+            custom_preview(screen_buf=screen, clip_object=keyframes[play_head]["clip_object"],
+                           fps=frame_rate, add_key_frame=False,
+                           duration=keyframes[play_head]["duration"])
+            frames_elapsed = (time.time() - t_before_clip) / step
+            play_head += round(frames_elapsed)
+        else:
+            play_head += 1
+            time.sleep(step)
 
 
 def HandleEvent(event):
@@ -137,24 +127,26 @@ def HandleEvent(event):
     # print(event.key)
     if event.key in clip_keys:
         index = clip_keys[event.key]
-        custom_preview(screen=screen, clip=clips[index], first_frame=clips_first_frame[index], fps=frame_rate)
+        custom_preview(screen_buf=screen, clip_object=clips[index], fps=frame_rate)
 
 
 videos_list = tk.Listbox(root)
 LoadFolder("vid")
-for i in range(len(clip_names)):
-    videos_list.insert(i, clip_names[i][clip_names[i].rfind("\\")+1:])
+for i in range(len(clips)):
+    vid_name = clips[i]["name"]
+    vid_name = vid_name[vid_name.rfind("\\")+1:]
+    videos_list.insert(i, vid_name)
     videos_list.pack()
 
 
 pg.display.set_caption('Video')
-screen = pg.display.set_mode(clips[0].size, 0)
+screen = pg.display.set_mode(clips[0]["clip"].size, 0)
 pg.display.init()
 pg.display.update()
 
-rec_btn = tk.Button(root,text="Rec", command=StartRecording)
+rec_btn = tk.Button(root, text="Rec", command=StartRecording)
 rec_btn.pack()
-play_btn = tk.Button(root,text="Play", command=PlayRecording)
+play_btn = tk.Button(root, text="Play", command=PlayRecording)
 play_btn.pack()
 
 while True:
